99e301e95430913ce1cd3365e28a878a98601ded
CodrRepo <owner@CodrRepo.com>
2024-05-02 13:40:05 +0530

    add some analytical tools

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {BrowserRouter} from 'react-router-dom'
-import Store from './store/Store.jsx'
-import {Provider} from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import Store from "./store/Store.jsx";
+import { Provider } from "react-redux";
+import { Analytics } from "@vercel/analytics/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={Store}>
     <BrowserRouter>
-    <App />
-  </BrowserRouter>
+      <Analytics>
+        <App />
+      </Analytics>
+    </BrowserRouter>
   </Provider>
-)
+);
