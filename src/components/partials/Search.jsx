34663555fb758a212df17bbd4c283c4773cc61b6
CodrRepo <owner@CodrRepo.com>
2024-05-02 14:44:41 +0530

    solve routing problem

--- src/components/partials/Search.jsx
+++ src/components/partials/Search.jsx
@@ -53,7 +53,7 @@ const Search = () => {
             className={`backdrop-blur  bg-[#8080801e] mt-2 overflow-auto absolute top-[100%] left-50% w-full max-h-[30vh] md:max-h-[40vh]`}
           >
             <div ref={suggestionBox} onWheel={handleWheel} className={`text-white flex flex-col gap-[1.3vh] z-40 `}>
-                {searchData && searchData.map((element, index)=><Link to={`${element.media_type}/details/${element.id}`} key={index} className="hover:backdrop-blur-lg w-full py-2 px-1 pl-4 flex items-center gap-3">
+                {searchData && searchData.map((element, index)=><Link to={window.location.pathname === '/'? `${element.media_type}/details/${element.id}`: `${window.location.origin}/${element.media_type}/details/${element.id}`} key={index} className="hover:backdrop-blur-lg w-full py-2 px-1 pl-4 flex items-center gap-3">
                   <span className="rounded-md w-[25%] md:w-[20%] h-[5vh] md:h-[3vw] lg:h-[4vw] inline-block">
                     <img className='h-full w-full object-cover object-center' src={(element.backdrop_path || element.poster_path || element.profile_path) != null ? `https://image.tmdb.org/t/p/original/${element.backdrop_path || element.poster_path || element.profile_path}`: `https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png`} alt="" />
                   </span>
